f5d5775c190c422d75188fd4a8755289d626322d
noIrLv <spore125@gmail.com>
2023-05-02 02:15:48 +0300

    initial commit

---
+++ src/test/java/page/LoginPage.java
@@ -0,0 +1,35 @@
+package page;
+
+import com.codeborne.selenide.Condition;
+import data.DataHelper;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+
+public class LoginPage {
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $("[data-test-id=login] input").setValue(info.getLogin());
+        $("[data-test-id=password] input").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+        return page(VerificationPage.class);
+    }
+
+    public void verifyErrorNotification() {
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible);
+    }
+
+    public void clickButton2Times() {
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=action-login]").click();
+    }
+
+    public void blockedSystemNotification() {
+        $("[data-test-id=error-notification]")
+                .shouldHave(text("Доступ заблокирован"))
+                .shouldBe(Condition.visible);
+    }
+}
---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,75 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+import page.VerificationPage;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanDatabase;
+
+public class LoginTest {
+    LoginPage loginPage = new LoginPage();
+    VerificationPage verificationPage = new VerificationPage();
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        cleanDatabase();
+    }
+
+    @Test
+    void shouldLoginValidData() {
+//        loginPage = open("http://localhost:9999/", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestDataUser1();
+        loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPage();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldErrorIfUserNotInDB() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification();
+    }
+
+    @Test
+    void shouldGetErrorIfInvalidPassword() {
+        var authInfo = DataHelper.getAuthInfoWithIncorrectPass();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification();
+    }
+
+    @Test
+    void shouldGetErrorIfIncorrectCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestDataUser2();
+        loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPage();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode);
+        verificationPage.verifyErrorNotification();
+    }
+
+    @Test
+    void shouldBlockWhenThreeInvalidPasswords() {
+        var authInfo = DataHelper.getAuthInfoWithIncorrectPass();
+        loginPage.validLogin(authInfo);
+        loginPage.clickButton2Times();
+        loginPage.blockedSystemNotification();
+
+    }
+
+
+}
